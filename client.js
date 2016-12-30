6feac9e8cb9d9a381131e68c6d99731ec53dfc33
nms377 <nms377@nyu.edu>
2016-12-29 17:40:09 -1000

    Added on to broadcast Client's message.

--- client.js
+++ client.js
@@ -2,9 +2,13 @@ const net = require('net');
 
 // v----- socket connection to server
 let server = net.createConnection(6969, 'localhost');
-server.on('connect', () => {
-	console.log('connected');
+server.on('connect', (chunk) => {
+	console.log('connected to ', server.address());
 
 	process.stdin.pipe(server);
 
+});
+
+server.on('data', (chunk) =>{
+	console.log(`SERVER: ${chunk}`);
 });
